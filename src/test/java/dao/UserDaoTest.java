cef1c26e8de765a4ea480ed26b92daf47005a0b0
sanghee <wjdtkdgml7352@daum.net>
2022-10-20 14:26:16 +0900

    [개선] BeforeEach 적용

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -2,10 +2,12 @@ package dao;
 
 import domain.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,11 +19,21 @@ import java.sql.SQLException;
 class UserDaoTest {
     @Autowired
     ApplicationContext context;
+    UserDao userDao;
+    User user1;
+    User user2;
+    User user3;
+    @BeforeEach
+    void setUp(){ //Bean을 가져오기 전에 셋업하는 부분( 코드 중복 제거 )
+//        userDao = context.getBean("awsUserDao", UserDao.class);
+        user1 = new User("1", "정상희", "1106");
+        user2 = new User("2", "신지원", "0129");
+        user3 = new User("3", "박정현", "0308");
+    }
+
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        User user1 = new User("1", "sanghee", "1106");
 
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         userDao.deleteAll();
         Assertions.assertEquals(0, userDao.getCount());
 
@@ -36,11 +48,7 @@ class UserDaoTest {
     }
 
     @Test void count() throws SQLException, ClassNotFoundException {
-        User user1 = new User("1", "정상희", "1106");
-        User user2 = new User("2", "신지원", "0129");
-        User user3 = new User("3", "박정현", "0308");
 
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         userDao.deleteAll();
         Assertions.assertEquals(0, userDao.getCount());
 
@@ -50,5 +58,13 @@ class UserDaoTest {
         Assertions.assertEquals(2, userDao.getCount());
         userDao.add(user1);
         Assertions.assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    void findById() throws SQLException, ClassNotFoundException {
+        Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+            userDao.findById("30");
+        });
+
     }
 }
